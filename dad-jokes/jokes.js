8a5da3c3015fa34acfbfab7b74fb98fb7d03ae4e
Hyunji Kim <creative.hkim@gmail.com>
2021-05-03 13:33:13 -0400

    adding new project

---
+++ dad-jokes/jokes.js
@@ -0,0 +1,14 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
